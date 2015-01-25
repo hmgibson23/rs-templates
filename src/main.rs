05fa6b17407c64a1e0e23bb7d1d951d442ada784
Hugo Gibson <gibsonhugo@gmail.com>
2015-01-25 16:07:49 +0000

    Release a token

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,52 @@
 
 
-mod lexer {
+mod Lexer {
     use std::io::{BufferedReader, File};
     use std::char::CharExt;
 
     /* The lexer that lexes */
     enum Token {
-        tok_value(String),
+        Expr(String),
         Missing
     }
 
-    fn getToken(input: &str) -> Token {
+    impl Token {
+        pub fn stringify(self) -> String {
+            match self {
+                Token::Expr(val) => val,
+                Missing => format!("No token.")
+            }
+        }
+    }
 
-        let mut lastChar = String::new();
+    pub fn get_token(input: &str) -> Token {
+
+        let mut last_char = String::new();
         let mut token = Token::Missing;
 
         let mut tokenizing = 0; //state machine for tokenizing
 
         for c in input.as_slice().chars() {
 
-            if(CharExt::is_whitespace(c)) {
+            if CharExt::is_whitespace(c) {
                 continue;
             }
 
-            match lastChar.find_str("}}") {
-                Some(uint) => { token = getExpr(lastChar) },
-                Noe => continue
+            match last_char.find_str("}}") {
+                Some(uint) => { token = get_expr(last_char.clone()) },
+                None => continue
 
             }
 
-            if(tokenizing == 1) {
+            if tokenizing == 1 {
                 // keep going until we get to the end of the token stream
-                lastChar.push(c);
+                last_char.push(c);
                 continue;
             }
 
-            if(c == '{') {
+            if c == '{' {
                 // assume it's an expression and tokenize it
-                lastChar.push(c);
+                last_char.push(c);
                 // we are now tokenizing
                 tokenizing = 1;
                 continue;
@@ -46,12 +55,13 @@ mod lexer {
         token
     }
 
-    fn getExpr(input: String) -> Token {
-        Token::Missing
+    fn get_expr(input: String) -> Token {
+        Token::Expr(input)
     }
 
 }
 
 fn main() {
-    println!("Hello, world!");
+    let token = Lexer::get_token("<html>{{value}}</html>");
+    println!("Got: {}", token.stringify())
 }
--- target/rs-templates.d
+++
@@ -1,4 +0,0 @@
-/home/hugo/git/rs-templates/target/rs-templates: src/main.rs
-
-/home/hugo/git/rs-templates/target/rs-templates.d: src/main.rs
-
