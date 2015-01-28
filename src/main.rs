173dff9b647923bc1eb368b99c8b7d0f6f570e41
hmgibson23 <hugo.gibson@yahoo.co.uk>
2015-01-28 14:53:47 +0000

    Parser kinda ready

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod parser;
 
 fn main() {
     let tokens = lexer::get_token("<html><p>{{value}}</p><p>{{another value}}</p></html>");
-//    let parsed = parser::map_tokens(tokens.clone());
+    let parsed = parser::map_tokens(tokens.clone());
 
 
     for t in tokens.map_in_place(|&: x: lexer::Token| -> String { x.stringify() }).iter() {
