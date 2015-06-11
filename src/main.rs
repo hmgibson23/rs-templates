8a958dd83dbd70f59b1b3c41f31004eeef0dd90f
hmgibson23 <hugo.gibson@yahoo.co.uk>
2015-06-11 10:04:57 +0100

    Mostly finalised

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ extern crate templates;
 use templates::{parser, util};
 
 use std::env;
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write};
 
 
 fn main() {
@@ -13,12 +15,21 @@ fn main() {
         std::process::exit(1);
     }
 
-    let input_file = env.nth(0);
+    let input_file = env.nth(0).unwrap();
     let data_file = env.nth(1);
 
-    let values = parser::parse_file(&input_file.unwrap());
+    let values = parser::parse_file(&input_file);
     let json = util::parse_json_file(&data_file.unwrap());
 
-    //map the values
+    let mut file = File::open(&input_file.clone()).unwrap();
+    let mut data = String::new();
 
+    file.read_to_string(&mut data).unwrap();
+
+
+    //map the values
+    let done = util::do_replace(&values, &json, &data);
+
+    let mut out_file = OpenOptions::new().write(true).open(&input_file.clone()).unwrap();
+    out_file.write_all(&done.into_bytes()).unwrap()
 }
